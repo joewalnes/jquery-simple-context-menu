d81ffc0d860ed074fcf822f7c8db1483665364b0
Joshua Linux Work <joshua@xmeausures.com>
2015-02-13 17:13:05 -0500

    Several Changes / Updates
    - can now specify the tag type that the clickable context-menu element should be. Defaults to <span> instead of <a>, since clicks don't necessarily take someone to a new resource/URI
    
    - class 'linkClicker' given to that clickable element, which allows for more accurate styling as well as DOM node traversal when building the menu (no need to rely on specific tags in a specific order; classes are more flexible)
    
    - ability to take "data-" attributes from the original target / the element the context-menu is bound to and have it associated with the clickable context-menu element
    
    - update jQuery methods / replaced .bind() with .on()
    
    - didn't touch any css manipulation/style manipulation ; presumably that can be refactored too at some point

--- jquery.contextmenu.js
+++ jquery.contextmenu.js
@@ -1,115 +1,113 @@
 /**
- * jQuery plugin for Pretty looking right click context menu.
+ * Forked by Joshua from  https://github.com/joewalnes/jquery-simple-context-menu
  *
- * Requires popup.js and popup.css to be included in your page. And jQuery, obviously.
- *
- * Usage:
- *
- *   $('.something').contextPopup({
- *     title: 'Some title',
- *     items: [
- *       {label:'My Item', icon:'/some/icon1.png', action:function() { alert('hi'); }},
- *       {label:'Item #2', icon:'/some/icon2.png', action:function() { alert('yo'); }},
- *       null, // divider
- *       {label:'Blahhhh', icon:'/some/icon3.png', action:function() { alert('bye'); }, isEnabled: function() { return false; }},
- *     ]
- *   });
- *
- * Icon needs to be 16x16. I recommend the Fugue icon set from: http://p.yusukekamiyamane.com/ 
- *
- * - Joe Walnes, 2011 http://joewalnes.com/
- *   https://github.com/joewalnes/jquery-simple-context-menu
+ * Now at
  *
  * MIT License: https://github.com/joewalnes/jquery-simple-context-menu/blob/master/LICENSE.txt
  */
 jQuery.fn.contextPopup = function(menuData) {
-	// Define default settings
-	var settings = {
-		contextMenuClass: 'contextMenuPlugin',
-		gutterLineClass: 'gutterLine',
-		headerClass: 'header',
-		seperatorClass: 'divider',
-		title: '',
-		items: []
-	};
-	
-	// merge them
-	$.extend(settings, menuData);
+    // Define default settings
+    //items should match dataTokenLookups!!
+    var settings = {
+        contextMenuClass: 'contextMenuPlugin',
+        fauxLinkTag: 'span',
+        fauxLinkAdditionalClass: 'contextMenuLink',
+        gutterLineClass: 'gutterLine',
+        headerClass: 'header',
+        seperatorClass: 'divider',
+        title: '',
+        items: [],
+        dataTokenLookups: []
+    };
 
-  // Build popup menu HTML
-  function createMenu(e) {
-    var menu = $('<ul class="' + settings.contextMenuClass + '"><div class="' + settings.gutterLineClass + '"></div></ul>')
-      .appendTo(document.body);
-    if (settings.title) {
-      $('<li class="' + settings.headerClass + '"></li>').text(settings.title).appendTo(menu);
-    }
-    settings.items.forEach(function(item) {
-      if (item) {
-        var rowCode = '<li><a href="#"><span></span></a></li>';
-        // if(item.icon)
-        //   rowCode += '<img>';
-        // rowCode +=  '<span></span></a></li>';
-        var row = $(rowCode).appendTo(menu);
-        if(item.icon){
-          var icon = $('<img>');
-          icon.attr('src', item.icon);
-          icon.insertBefore(row.find('span'));
+    // merge them
+    $.extend(settings, menuData);
+
+
+    // Build popup menu HTML
+    function createMenu(e) {
+        var menu = $('<ul class="' + settings.contextMenuClass + '"><div class="' + settings.gutterLineClass + '"></div></ul>').appendTo(document.body);
+        if (settings.title) {
+            $('<li class="' + settings.headerClass + '"></li>').text(settings.title).appendTo(menu);
         }
-        row.find('span').text(item.label);
-          
-        if (item.isEnabled != undefined && !item.isEnabled()) {
-            row.addClass('disabled');
-        } else if (item.action) {
-            row.find('a').click(function () { item.action(e); });
+        settings.items.forEach($.proxy(function(item,i) {
+            if (item) {
+                var rowCode = '<li><'+settings.fauxLinkTag+' class="'+settings.fauxLinkAdditionalClass+ ' linkClicker"><span class="itemTitle"></span></'+settings.fauxLinkTag+'></li>';
+                // if(item.icon)
+                //   rowCode += '<img>';
+                // rowCode +=  '<span></span></a></li>';
+                var row = $(rowCode).appendTo(menu);
+                if(item.icon){
+                    var icon = $('<img>');
+                    icon.attr('src', item.icon);
+                    icon.insertBefore(row.find('.itemTitle'));
+                }
+                row.find('.itemTitle').text(item.label);
+
+                if (typeof(item.isEnabled)!=='undefined' && !item.isEnabled()) {
+                    row.addClass('disabled');
+                } else if (item.action) {
+                    row.find('.linkClicker').on('click',function () {
+                        item.action(e);
+                    });
+                }
+
+            } else {
+                $('<li class="' + settings.seperatorClass + '"></li>').appendTo(menu);
+            }
+        },this));
+        menu.find('.' + settings.headerClass ).text(settings.title);
+        var originalTarget = $(event.target);
+        if(settings.dataTokenLookups.length > 0) {
+            for (var xi = 0; xi < settings.dataTokenLookups.length; xi++) {
+                var lookupTokenName = settings.dataTokenLookups[xi];
+                var el = $(menu.find('.linkClicker').get([xi]));
+                var dataVal = originalTarget.attr(lookupTokenName);
+                el.attr('data-token', dataVal);
+            }
+        }
+        return menu;
+    }
+
+
+    // On contextmenu event (right click)
+    this.on('contextmenu', function(e) {
+        var menu = createMenu(e).show();
+
+        var left = e.pageX + 5, /* nudge to the right, so the pointer is covering the title */
+            top = e.pageY;
+        if (top + menu.height() >= $(window).height()) {
+            top -= menu.height();
+        }
+        if (left + menu.width() >= $(window).width()) {
+            left -= menu.width();
         }
 
-      } else {
-        $('<li class="' + settings.seperatorClass + '"></li>').appendTo(menu);
-      }
-    });
-    menu.find('.' + settings.headerClass ).text(settings.title);
-    return menu;
-  }
+        // Create and show menu
+        menu.css({zIndex:1000001, left:left, top:top})
+            .on('contextmenu', function() { return false; });
 
-  // On contextmenu event (right click)
-  this.bind('contextmenu', function(e) {	
-    var menu = createMenu(e)
-      .show();
-    
-    var left = e.pageX + 5, /* nudge to the right, so the pointer is covering the title */
-        top = e.pageY;
-    if (top + menu.height() >= $(window).height()) {
-        top -= menu.height();
-    }
-    if (left + menu.width() >= $(window).width()) {
-        left -= menu.width();
-    }
+        // Cover rest of page with invisible div that when clicked will cancel the popup.
+        var bg = $('<div></div>')
+            .css({left:0, top:0, width:'100%', height:'100%', position:'absolute', zIndex:1000000})
+            .appendTo(document.body)
+            .on('contextmenu click', function() {
+                // If click or right click anywhere else on page: remove clean up.
+                bg.remove();
+                menu.remove();
+                return false;
+            });
 
-    // Create and show menu
-    menu.css({zIndex:1000001, left:left, top:top})
-      .bind('contextmenu', function() { return false; });
+        // When clicking on a link in menu: clean up (in addition to handlers on link already)
+        menu.find('.linkClicker').on('click',function() {
+            bg.remove();
+            menu.remove();
+        });
 
-    // Cover rest of page with invisible div that when clicked will cancel the popup.
-    var bg = $('<div></div>')
-      .css({left:0, top:0, width:'100%', height:'100%', position:'absolute', zIndex:1000000})
-      .appendTo(document.body)
-      .bind('contextmenu click', function() {
-        // If click or right click anywhere else on page: remove clean up.
-        bg.remove();
-        menu.remove();
+        // Cancel event, so real browser popup doesn't appear.
         return false;
-      });
-
-    // When clicking on a link in menu: clean up (in addition to handlers on link already)
-    menu.find('a').click(function() {
-      bg.remove();
-      menu.remove();
     });
 
-    // Cancel event, so real browser popup doesn't appear.
-    return false;
-  });
-
-  return this;
+    return this;
 };
 
