7361b5a6dd0c9ee2442b3e784b2c05418aaf2f3c
Joshua Linux Work <joshua@xmeausures.com>
2015-02-17 09:57:45 -0500

    Updated jQuery methods / replaced .bind() with .on() and linked to an updated jQuery version (1.11.2) in example.html

--- jquery.contextmenu.js
+++ jquery.contextmenu.js
@@ -72,7 +72,7 @@ jQuery.fn.contextPopup = function(menuData) {
   }
 
   // On contextmenu event (right click)
-  this.bind('contextmenu', function(e) {	
+  this.on('contextmenu', function(e) {
     var menu = createMenu(e)
       .show();
     
@@ -87,13 +87,13 @@ jQuery.fn.contextPopup = function(menuData) {
 
     // Create and show menu
     menu.css({zIndex:1000001, left:left, top:top})
-      .bind('contextmenu', function() { return false; });
+      .on('contextmenu', function() { return false; });
 
     // Cover rest of page with invisible div that when clicked will cancel the popup.
     var bg = $('<div></div>')
       .css({left:0, top:0, width:'100%', height:'100%', position:'absolute', zIndex:1000000})
       .appendTo(document.body)
-      .bind('contextmenu click', function() {
+      .on('contextmenu click', function() {
         // If click or right click anywhere else on page: remove clean up.
         bg.remove();
         menu.remove();
